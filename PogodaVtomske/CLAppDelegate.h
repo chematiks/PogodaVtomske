61db528a33d1c238ad7ec96ad32c4aab13bf18b6
Администратор <administrator@Administrators-Mac.local>
2013-08-17 10:30:39 +0400

    Initial Commit

---
+++ PogodaVtomske/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  PogodaVtomske
+//
+//  Created by Администратор on 8/17/13.
+//  Copyright (c) 2013 ChematiksLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PogodaVtomske/CLViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLViewController.h
+//  PogodaVtomske
+//
+//  Created by Администратор on 8/17/13.
+//  Copyright (c) 2013 ChematiksLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLViewController : UIViewController
+
+@end
